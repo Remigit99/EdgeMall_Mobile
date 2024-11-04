75bd85040dc20052a4ad0e2cac702360fd640522
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-11-04 16:21:28 +0100

    fetch data from EdgeMall Backend server

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -1,8 +1,34 @@
-import { Image, ImageBackground, ScrollView, StyleSheet, Switch, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, FlatList, Image, ImageBackground, ScrollView, StyleSheet, Switch, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Entypo from '@expo/vector-icons/Entypo';
+import axios from "axios"
 
 const Home = () => {
+
+    const [shoes, setShoes] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    const fetchData = async() =>{
+setIsLoading(true)
+        try {
+            const data = await axios.get("http://localhost:3000/api/v1/products")
+            console.log(data)
+            setShoes(data)
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error)
+            setIsLoading(false)
+        }
+    }
+
+    useEffect( () =>{
+        fetchData()
+    }, [])
+
+
+
     return (
         <SafeAreaView>
             <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginVertical: 16, marginHorizontal: 16 }}>
@@ -10,8 +36,8 @@ const Home = () => {
                 <View style={{ flexDirection: "row", gap: 16, alignItems: "center" }}>
                     <Switch />
                     <View style={{ width: 28, height: 28, borderRadius: 14, backgroundColor: "gray" }}>
-                        <Image source={require("../../assets/images/user.svg")} style={{ display: "flex", justifySelf: "center", alignSelf: "center" }} />
-
+                        {/* <Image source={require("../../assets/images/user.svg")} style={{ display: "flex", justifySelf: "center", alignSelf: "center" }} /> */}
+                        <EvilIcons name="search" size={24} color="white" />
                         {/* <Text>Search</Text> */}
                     </View>
                 </View>
@@ -71,6 +97,33 @@ const Home = () => {
                 </View>
 
                 <View>
+                    { isLoading ? <ActivityIndicator size={'large'} color={"coral"}/> :
+
+
+// JSON.stringify(shoes)
+                        // shoes.map(({item}) =>{
+                        //     <View> 
+                        //     <Text>{item.title}</Text>
+                        //     <Image source={{uri: item.productImgUrl}} style={{width:100}}/>
+                        // </View>
+                        // })
+
+                        <FlatList  
+                            data={shoes}
+                            renderItem={({item}) => 
+                                <View> 
+                                 <Text>{item.title}</Text>
+                                 <Image source={{uri: item.productImgUrl}} style={{width:100}}/>
+                             </View>
+                            }
+
+                            keyExtractor={item => item._id}
+                        />
+                        
+                    }
+                </View>
+
+                <View>
                     
                     <View style={{flexDirection:"row", gap: 14, alignItems: "center", marginHorizontal: 16}}>
                         <View>
@@ -86,7 +139,7 @@ const Home = () => {
                             </View>
 
                             <View>
-                                <Text>Saved</Text>
+                                <Entypo name="bookmark" size={24} color="black" />
                             </View>
                         </View>
 
@@ -106,7 +159,7 @@ const Home = () => {
                             </View>
 
                             <View>
-                                <Text>Saved</Text>
+                                <Entypo name="bookmark" size={24} color="black" />
                             </View>
                         </View>
 
@@ -126,7 +179,7 @@ const Home = () => {
                             </View>
 
                             <View>
-                                <Text>Saved</Text>
+                                <Entypo name="bookmark" size={24} color="black" />
                             </View>
                         </View>
 
@@ -146,7 +199,7 @@ const Home = () => {
                             </View>
 
                             <View>
-                                <Text>Saved</Text>
+                                <Entypo name="bookmark" size={24} color="black" />
                             </View>
                         </View>
 
@@ -166,7 +219,7 @@ const Home = () => {
                             </View>
 
                             <View>
-                                <Text>Saved</Text>
+                                <Entypo name="bookmark" size={24} color="black" />
                             </View>
                         </View>
 
